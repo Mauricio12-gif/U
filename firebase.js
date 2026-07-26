ed2faee0d75e878a2a548a1be13ee3e00dd031cc
Mauricio12-gif <lovermax876@gmail.com>
2026-07-26 08:56:43 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,3 @@
-// Import Firebase
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 
 import { 
@@ -7,52 +5,41 @@ getFirestore
 } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 
-import {
-getStorage
-} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-storage.js";
+import { 
+getAuth 
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 
 
-
-// Firebase configuration
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyAWUws3p5ukwwo2eLxnHV8YJ1dBFNGSWcI",
+apiKey: "AIzaSyAWUws3p5ukwwo2eLxnHV8YJ1dBFNGSWcI",
 
-  authDomain: "love-dbc36.firebaseapp.com",
+authDomain: "love-dbc36.firebaseapp.com",
 
-  projectId: "love-dbc36",
+projectId: "love-dbc36",
 
-  storageBucket: "love-dbc36.firebasestorage.app",
+storageBucket: "love-dbc36.firebasestorage.app",
 
-  messagingSenderId: "325938096453",
+messagingSenderId: "325938096453",
 
-  appId: "1:325938096453:web:ee25ec17f024c68b51362c",
+appId: "1:325938096453:web:ee25ec17f024c68b51362c",
 
-  measurementId: "G-ME0MEC2CJ8"
+measurementId: "G-ME0MEC2CJ8"
 
 };
 
 
 
-// Start Firebase
-
 const app = initializeApp(firebaseConfig);
 
 
 
-// Connect Firestore
-
 const db = getFirestore(app);
 
 
-
-// Connect Storage
-
-const storage = getStorage(app);
+const auth = getAuth(app);
 
 
 
-// Export for other files
-
-export { db, storage };
+export { db, auth };
