d74fb5c358d715103370ae3a08a03799d0ec7bc3
Mauricio12-gif <lovermax876@gmail.com>
2026-07-26 05:58:01 +0300

    Refactor error handling and user messaging

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ import {
 collection,
 addDoc,
 onSnapshot,
-query,
-where,
 serverTimestamp
 } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
@@ -25,40 +23,46 @@ document.getElementById("visitorName").value.trim();
 const password =
 document.getElementById("password").value;
 
+const error =
+document.getElementById("error");
+
+
 
 if(name===""){
 
-document.getElementById("error").innerHTML =
-"Enter your name ❤️";
-
+error.innerHTML="Enter your name ❤️";
 return;
 
 }
+
 
 
 if(password!=="LOVE"){
 
-document.getElementById("error").innerHTML =
-"Wrong password ❤️";
-
+error.innerHTML="Wrong password ❤️";
 return;
 
 }
+
 
 
 currentUser = name;
 
 
 
-document.getElementById("loginPage")
+document
+.getElementById("loginPage")
 .classList.add("hidden");
 
 
-document.getElementById("mainPage")
+document
+.getElementById("mainPage")
 .classList.remove("hidden");
 
 
-document.getElementById("welcome")
+
+document
+.getElementById("welcome")
 .innerHTML =
 "Welcome " + name + " ❤️";
 
@@ -75,12 +79,14 @@ loadMessages();
 
 
 
-// OPEN SECTIONS
 
-window.showSection=function(id){
+// SHOW SECTIONS
+
+window.showSection=function(sectionID){
 
 
-document.querySelectorAll(".content")
+document
+.querySelectorAll(".content")
 .forEach(section=>{
 
 section.classList.add("hidden");
@@ -88,12 +94,14 @@ section.classList.add("hidden");
 });
 
 
-document.getElementById(id)
+
+document
+.getElementById(sectionID)
 .classList.remove("hidden");
 
 
 
-if(id==="chat"){
+if(sectionID==="chat"){
 
 loadMessages();
 
@@ -134,11 +142,13 @@ collection(db,"messages"),
 
 {
 
-sender:currentUser,
+ChatID: currentUser,
 
-message:text,
+Message: text,
 
-time:serverTimestamp()
+Sender: currentUser,
+
+Time: serverTimestamp()
 
 }
 
@@ -181,7 +191,8 @@ box.innerHTML="";
 snapshot.forEach(doc=>{
 
 
-let data = doc.data();
+const data = doc.data();
+
 
 
 
@@ -192,15 +203,15 @@ if(currentUser.toLowerCase()==="mauricio"){
 
 box.innerHTML += `
 
-<div>
+<p>
 
-<b>❤️ ${data.sender}</b>
+<b>❤️ ${data.Sender}</b>
 
 <br>
 
-${data.message}
+${data.Message}
 
-</div>
+</p>
 
 <hr>
 
@@ -212,22 +223,23 @@ ${data.message}
 
 
 
+
 // OTHER USERS ONLY SEE THEIR OWN
 
-else if(data.sender===currentUser){
+else if(data.ChatID === currentUser){
 
 
 box.innerHTML += `
 
-<div>
+<p>
 
 <b>❤️ You</b>
 
 <br>
 
-${data.message}
+${data.Message}
 
-</div>
+</p>
 
 <hr>
 
@@ -260,11 +272,12 @@ ${data.message}
 window.openWhatsApp=function(){
 
 
-let phone="254797147255";
+let phone="254797147155";
 
 
-let message=
+let message =
 "Hello Mauricio ❤️ I visited your website.";
+
 
 
 let link =
@@ -277,7 +290,24 @@ phone
 encodeURIComponent(message);
 
 
+
 window.open(link,"_blank");
 
+
+};
+
+
+
+
+
+
+
+
+
+// LOGOUT
+
+window.logout=function(){
+
+location.reload();
 
 };
