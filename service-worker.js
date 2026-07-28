e6eae96900a60a52c865c7c46a7cc19ffc2de893
Mauricio12-gif <lovermax876@gmail.com>
2026-07-28 12:35:32 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -31,10 +31,22 @@ self.addEventListener("fetch", event => {
 
     event.respondWith(
 
-        caches.match(event.request)
+        fetch(event.request)
         .then(response => {
 
-            return response || fetch(event.request);
+            return caches.open(CACHE_NAME)
+            .then(cache => {
+
+                cache.put(event.request, response.clone());
+
+                return response;
+
+            });
+
+        })
+        .catch(() => {
+
+            return caches.match(event.request);
 
         })
 
